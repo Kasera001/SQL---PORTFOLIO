efbb9443e230a5cff209686522c919b6d4bcdbc1
Kasera001 <kaserastella1@gmail.com>
2026-07-27 14:09:24 +0300

    CHAGNGES TO THE TEAM AND STADIUM TABLE

--- Premier-League-Analysis/create_tables.sql
+++ Premier-League-Analysis/create_tables.sql
@@ -1,19 +1,19 @@
-CREATE TABLE teams (
-    team_id INT NOT NULL AUTO_INCREMENT,
-    team_name VARCHAR(100) NOT NULL,
-    stadium_id INT,
-    city VARCHAR(100),
-    founded_year YEAR,
-    PRIMARY KEY (team_id)
-);
-
-CREATE TABLE stadiums
-(
+CREATE TABLE stadiums(
     stadium_id INT NOT NULL AUTO_INCREMENT,
     stadium_name VARCHAR(100) NOT NULL,
     capacity INT,
     city VARCHAR(100),
     PRIMARY KEY (stadium_id)
+);
+
+
+CREATE TABLE teams (
+    team_id INT NOT NULL AUTO_INCREMENT,
+    team_name VARCHAR(100) NOT NULL,
+    stadium_id INT FOREIGN KEY REFERENCES stadiums(stadium_id),
+    city VARCHAR(100),
+    founded_year YEAR,
+    PRIMARY KEY (team_id)
 );
 
 CREATE TABLE  managers (
